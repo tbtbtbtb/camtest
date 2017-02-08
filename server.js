d98bed62326fae340adc73383a3d987c07768337
tbtbtbtb <tbtbtbtb@users.noreply.github.com>
2017-02-08 09:03:50 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var httpProxy = require('http-proxy');
+var proxy = httpProxy.createProxyServer({});
+
+http.createServer(function(req, res) {
+    proxy.web(req, res, { target: process.env.CAMERA_URI });
+}).listen(8080);
