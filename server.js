43b1f813673bc73a42143669cc636795eeeaccea
tbtbtbtb <tbtbtbtb@users.noreply.github.com>
2017-02-16 10:44:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ var httpProxy = require('http-proxy');
 const port = 8080;
 var proxy = httpProxy.createProxyServer({});
 
-var requestHandler = (request, response) => {  
-    proxy.web(request, response, { target: process.env.CAMERA_URI });
-    console.log(request.url + ' => ' + process.env.CAMERA_URI );
+var requestHandler = (request, response) => {
+  proxy.web(request, response, { target: process.env.CAMERA_URI });
+  console.log(request.url + ' => ' + process.env.CAMERA_URI );
 };
 
-var server = http.createServer(requestHandler).listen(port);
+var server = http.createServer(requestHandler);
+
+server.listen(port);
